59fef0f41ad992acf7542544fe302a5e24fd69ff
rogerbernar9 <roger.bernar9@gmail.com>
2020-11-26 13:14:22 -0300

    aulas 178, 179 e 180 - implementação do login parte 1

--- app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
+++ app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
@@ -4,12 +4,29 @@ import android.view.View
 import com.example.tasks.service.listener.APIListener
 import com.example.tasks.service.repository.remote.PersonService
 import com.example.tasks.service.repository.remote.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class PersonRepository {
 
-    val remote = RetrofitClient.createService(PersonService::class.java)
+    val mRemote = RetrofitClient.createService(PersonService::class.java)
 
-    fun login(email: String, password: String) {
+    fun login(email: String, password: String, listener: APIListener) {
+        val call: Call<HeaderModel> = mRemote.login(email, password)
+
+        call.enqueue(object: Callback<HeaderModel> {
+            override fun onResponse(call: Call<HeaderModel>, response: Response<HeaderModel>) {
+                response.body()?.let { listener.onSuccess(it) }
+
+            }
+
+            override fun onFailure(call: Call<HeaderModel>, t: Throwable) {
+                listener.onFailure(t.message.toString())
+            }
+
+        })
+        val s = ""
 
     }
 
