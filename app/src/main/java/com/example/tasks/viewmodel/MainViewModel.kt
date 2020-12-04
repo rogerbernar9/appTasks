acd8f95eeae4e7c33932b0507704c1498af1c92f
roger <roger.bernar9@gmail.com>
2020-12-04 09:10:02 -0300

    aula 199 - Navigation drawer pegando o nome do usuario

--- app/src/main/java/com/example/tasks/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/tasks/viewmodel/MainViewModel.kt
@@ -1,7 +1,25 @@
 package com.example.tasks.viewmodel
 
 import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.tasks.service.constants.TaskConstants
+import com.example.tasks.service.listener.ValidationListener
+import com.example.tasks.service.repository.local.SecurityPreferences
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val mSharedPreferences = SecurityPreferences(application)
+
+    private val mUserName = MutableLiveData<String>()
+    var username: LiveData<String> = mUserName
+
+    fun loadUserName() {
+        mUserName.value = mSharedPreferences
+            .get(TaskConstants.SHARED.PERSON_NAME)
+    }
+
 }
